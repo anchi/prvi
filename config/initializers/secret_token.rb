26594700acbdd409c5eef1e94f9bd34d1ce2fea8
ana <kali667@gmail.com>
2011-12-16 14:08:02 +0100

    prva verzija

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prvi::Application.config.secret_token = '529fb831c0f3b22e35e7f8dfc0ff43c88c255be0f6509614c80d6687336f54bb5fb7d05431a36f4f9eb2f8029531d4cb5b6c4351cc5260f8b08bbf76542bc992'
